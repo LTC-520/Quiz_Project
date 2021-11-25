3fbf2cac79c7b85fd0695954920bd3fa7d429f0d
Luis <LTCodes520@gmail.com>
2021-11-25 09:11:50 -0800

    Initial commit

--- Game_JS/game.js
+++ Game_JS/game.js
@@ -1,1 +1,10 @@
-console.log("Hello world");
+const question = document.getElementById("question");
+const choices = document.getElementsByClassName("choice-text");
+console.log(choices);
+
+let currentQuestion = {};
+let acceptingAnswers = true;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
