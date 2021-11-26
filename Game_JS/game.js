7d1b4f5eb7520590659d3c561e47ae3d20386292
Luis <LTCodes520@gmail.com>
2021-11-26 11:19:40 -0800

    correct/incorrect class syntaxes

--- Game_JS/game.js
+++ Game_JS/game.js
@@ -73,6 +73,16 @@ choices.forEach((choice) => {
         acceptingAnswers = false;
         const selectedChoice = e.target;
         const selectedAnswer = selectedChoice.dataset['number'];
+
+        /*const classToApply = 'incorrect';
+        if(selectedAnswer == currentQuestion.answer){
+            classToApply = 'correct';
+        }*/
+
+        const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+        console.log(classToApply);
+
+        selectedChoice.parentElement.classList.add(classToApply);
         getNewQuestion();
     });
 });
